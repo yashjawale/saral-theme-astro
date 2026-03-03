781a9c8e3b2d6cc100cbcdbc8021ac4f6dbc9a0c
nastasagr <alexanstgr@gmail.com>
2026-03-03 13:00:56 +0200

    filter button

---
+++ src/components/react/FilterBtn.tsx
@@ -0,0 +1,26 @@
+import { type ProjType } from '@/data/projects';
+
+interface FilterBtnProps {
+  type: ProjType;
+  active: boolean;
+  handle: () => void;
+}
+
+
+const FilterBtn: React.FC<FilterBtnProps> = ({ type, active, handle }) => {
+
+  const enabled  = "bg-primary text-white transition-fade duration-400 ease-in-out";
+  const disabled = "text-black dark:text-white dark:bg-white/20 bg-black/10";
+
+  return (  
+    <button
+      onClick={handle}
+      className={`px-2 py-1 text-xs md:text-lg lg:text-lg cursor-pointer rounded-md ${active ? enabled : disabled}`}
+    >
+      {type.charAt(0).toUpperCase() + type.slice(1)}
+    </button>
+  );
+};
+
+
+export default FilterBtn;
