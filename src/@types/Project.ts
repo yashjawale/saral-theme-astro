10a04c6e6ae3f123b8a6c51ddf8320a6538e1270
nastasagr <alexanstgr@gmail.com>
2026-03-03 11:29:25 +0200

    add project interface

---
+++ src/@types/Project.ts
@@ -0,0 +1,20 @@
+
+export const ProjTypes = ["all", "websites", "apps", "design"] as const;
+
+// cast projtype
+export type ProjType = typeof ProjTypes[number];
+
+export const StatusTypes = ["completed", "in-progress", "on-hold"] as const;
+
+export type StatusType = typeof StatusTypes[number];
+
+// actual project type 
+export interface Project {
+  title: string;
+  desc?: string;
+  url?: string;
+  repo?: string;
+  type: ProjType;
+  stack: string[];
+  status: StatusType;
+}
