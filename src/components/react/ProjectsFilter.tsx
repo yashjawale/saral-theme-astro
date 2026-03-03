ad0a4b6b2e13a7c9a7c197422821d9e9a727369b
nastasagr <alexanstgr@gmail.com>
2026-03-03 22:01:54 +0200

    remove unused code

--- src/components/react/ProjectsFilter.tsx
+++ src/components/react/ProjectsFilter.tsx
@@ -1,62 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { type ProjType, type Project } from '@/@types/Project';
+import { useState, useEffect } from 'react'
+import { type ProjType, type Project } from '@/@types/Project'
 
-import { ProjTypes } from '@/@types/Project';
-import { Projects } from '@/data/projects';
+import { ProjTypes } from '@/@types/Project'
+import { Projects } from '@/data/projects'
 
 // components
-import ProjectCard from './ProjectCard';
-import FilterBtn from './FilterBtn';
-
+import ProjectCard from './ProjectCard'
+import FilterBtn from './FilterBtn'
 
 export default function ProjectsFilter() {
+	const [selectedType, setSelectedType] = useState<ProjType>('all')
+	const [filteredProjects, setFilteredProjects] = useState<Project[]>([])
 
+	// updateFilter
+	const setFilter = (filter: ProjType | 'all') => {
+		setSelectedType(filter)
+	}
 
-    const [selectedType, setSelectedType] = useState<ProjType>("all");
-    const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
+	useEffect(() => {
+		if (selectedType === 'all') {
+			setFilteredProjects(Projects)
+		} else {
+			const filtered = Projects.filter(
+				(project) => project.type === selectedType
+			)
+			setFilteredProjects(filtered)
+		}
+	}, [selectedType])
+	return (
+		<div className="flex flex-col gap-12">
+			<div className="flex flex-wrap gap-2">
+				{ProjTypes.map((type) => (
+					<FilterBtn
+						type={type}
+						handle={() => {
+							setFilter(type)
+						}}
+						active={selectedType === type}
+					/>
+				))}
+			</div>
 
-
-    const NoProjectsGR = "Δεν βρέθηκαν projects σε αυτή την κατηγορία.";
-    const NoProjectsEN = "No projects found for the selected filter.";
-
-    // updateFilter
-    const setFilter = (filter: ProjType | "all") => {
-        setSelectedType(filter)
-    }
-
-
-    useEffect(() => {
-        if (selectedType === "all") {
-            setFilteredProjects(Projects);
-        } else {
-            const filtered = Projects.filter((project) => project.type === selectedType);
-            setFilteredProjects(filtered);
-        }
-    }, [selectedType]);
-    return (
-        <div className="flex flex-col gap-12">
-            <div className="flex flex-wrap gap-2">
-
-                {ProjTypes.map((type) => (
-                    <FilterBtn type={type} handle={() => { setFilter(type) }} active={selectedType === type} />
-                ))}
-
-            </div>
-
-
-
-            <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-3">
-                {filteredProjects.length > 0 ? (
-                    filteredProjects.map((project) => (
-                        <ProjectCard project={project} key={project.title} />
-                    ))
-                ) : (
-                    <p>No prjects in this category</p>
-                )}
-            </div>
-        </div>
-    );
+			<div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-3">
+				{filteredProjects.length > 0 ? (
+					filteredProjects.map((project) => (
+						<ProjectCard project={project} key={project.title} />
+					))
+				) : (
+					<p>No prjects in this category</p>
+				)}
+			</div>
+		</div>
+	)
 }
-
-
-
