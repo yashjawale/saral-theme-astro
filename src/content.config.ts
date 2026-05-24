6339ffd78e3f69fd4a91bdb3ff53f397697a873e
yashjawale <yash25.j@gmail.com>
2026-05-24 09:24:09 +0530

    refactor: update blog collection references and paths to use 'blogs' & update to ues content collections API

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,9 @@
 import { defineCollection } from 'astro:content'
 import { z } from 'astro/zod'
+import { glob } from 'astro/loaders'
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
+const blogs = defineCollection({
+	loader: glob({ base: 'src/content/blog', pattern: '*.{md,mdx}' }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
@@ -12,4 +13,4 @@ const blog = defineCollection({
 	}),
 })
 
-export const collections = { blog }
+export const collections = { blogs }
